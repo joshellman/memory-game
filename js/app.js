fb635e77250351dac2ba78c4892074e062d13d9d
Josh Ellman <josh.ellman@gmail.com>
2016-12-05 19:23:34 -0800

    Switches players and renders a winner.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
-// Need to create 2 player start buttons
-// Need to create logic to assign a winner
-// Cards are no longer flipping over when clicked
+// Need to Switch turns between two players
+// Need to Design logic for winning & visually display which player won
+// 2 player start buttons ?
 
 
 var $container = $('#container')
 var $timer = $('#timer')
 var pick1 = null
 var pick2 = null
-
-var player1Time = 0
-var player2Time = 0
+var $faces = null
 
 // all of our cards:
 var deck = ['<img src="css/lion.png" />', '<img src="css/lion.png" />', '<img src="css/gorilla.png" />', '<img src="css/gorilla.png" />', '<img src="css/elephant.png" />', '<img src="css/elephant.png" />', '<img src="css/tiger.png" />', '<img src="css/tiger.png" />', '<img src="css/hippo.png" />', '<img src="css/hippo.png" />' , '<img src="css/wolf.png" />', '<img src="css/wolf.png" />', '<img src="css/zebra.png" />', '<img src="css/zebra.png" />', '<img src="css/cheetah.png" />', '<img src="css/cheetah.png" />', '<img src="css/rhino.png" />', '<img src="css/rhino.png" />', '<img src="css/jaguar.png" />'
@@ -26,18 +24,19 @@ switchPlayer = function() {
   if (game.currentPlayer == game.player2) {
     game.currentPlayer = game.player1
   } else {
+    initializeGame()
     game.currentPlayer = game.player2
   }
 }
 var game = {
   player1: {
     name: "p1",
-    score: 0,
+    time: 0,
     matches: 0
   },
   player2: {
     name: "p2",
-    score: 0,
+    time: 0,
     matches: 0
   }
 }
@@ -70,64 +69,65 @@ function shuffle(deck) {
   return deck;
 }
 
-
-shuffle(deck)
-// deal the cards:
-for(var i = 0; i < deck.length; i += 1) {
-  $container.append('<div class="card "><div class="face">' + deck[i] + '</div></div>')
-}
-//collection of '.face' divs that we already
-var $faces = $(".face")
-
-
-// when you click a '.face':
-$faces.on('click', function(){
-  if(pick1 == null) {
-    pick1 = $(this)
-  } else {
-    pick2 = $(this)
-
-    // if they're equal, do stuff, otherwise, do other stuff.
-    if (pick1.html() == pick2.html()){
-      console.log("true")
-      game.currentPlayer.matches += 1
-
-      pick1.addClass('matched')
-      pick2.addClass('matched')
-
-      pick1 = null
-      pick2 = null
-
-      checkMatches()
-      // check how many have been matched
-      // if($('.matched').length == 20)
-      // $('.matched')
-      // // [div.face.matched, div.face.matched]
-      // $('.matched').length
+function initializeGame() {
+  $('#container').empty()
+  shuffle(deck)
+  // deal the cards:
+  for(var i = 0; i < deck.length; i += 1) {
+    $container.append('<div class="card "><div class="face">' + deck[i] + '</div></div>')
+  }
+  //collection of '.face' divs that we already
+  $faces = $(".face")
 
 
-} else {
-      console.log("false")
+  // when you click a '.face':
+  $faces.on('click', function(){
+    if(pick1 == null) {
+      pick1 = $(this)
+    } else {
+      pick2 = $(this)
 
-      setTimeout(function (){
-        pick1.animate({opacity: 0})
-        pick2.animate({opacity: 0})
+      // if they're equal, do stuff, otherwise, do other stuff.
+      if (pick1.html() == pick2.html()){
+        console.log("true")
+        game.currentPlayer.matches += 1
+
+        pick1.addClass('matched').off('click')
+        pick2.addClass('matched').off('click')
+
         pick1 = null
         pick2 = null
-      }, 500)
+
+        checkMatches()
+
+
+
+
+  } else {
+        console.log("false")
+
+        setTimeout(function (){
+          pick1.animate({opacity: 0})
+          pick2.animate({opacity: 0})
+          pick1 = null
+          pick2 = null
+        }, 500)
+
+      }
+
+      // but either way: set pick1 and pick2 back to null at the end of this else
 
     }
 
-    // but either way: set pick1 and pick2 back to null at the end of this else
+    // animate its transparency to '1'
+    $(this).animate({opacity: 1})
 
-  }
+    // print the text thats inside to the console
+    console.log($(this).text())
+  })
+}
 
-  // animate its transparency to '1'
-  $(this).animate({opacity: 1})
-
-  // print the text thats inside to the console
-  console.log($(this).text())
-})
+initializeGame()
 
 $(startButton).on('click', function() {
   showCards = setTimeout(function() {
@@ -140,8 +140,8 @@ $(startButton).on('click', function() {
   setTimeout(function() {
     Timer = setInterval(function() {
       // if($timer.html() > 0) {
-        player1Time += 1
-        $timer.text(player1Time)
+        game.currentPlayer.time += 1
+        $timer.text(game.currentPlayer.time)
       // }
     },1000);
   }, 5000);
@@ -175,13 +175,30 @@ $(startButton).on('click', function() {
 
 
 function checkMatches() {
-  if (game.currentPlayer.matches == 10) {
-    game.currentPlayer.score = $('#timer').html();
+  if($('.matched').length == 20) {
+    console.log("")
     clearInterval(Timer)
     alert("You found all matches!")
-    switchPlayer();
+    $('#Player1').text("Player 1 Time: " +game.player1.time)
+
+    if(game.currentPlayer == game.player1) {
+      switchPlayer();
+    } else {
+      console.log("Game Over!")
+        $('#Player2').text("Player 2 Time: " +game.player2.time)
+      // compare the scores
+      if(game.player1.time < game.player2.time) {
+        // player 1 wins
+        $('h1').text("Player 1 wins!")
+
+      } else {
+        // player 2 wins
+        $('h1').text("Player 2 wins!")
+      }
+    }
     $('#timer').text(0)
   }
+
 }
 // $('h1').text(    game.currentPlayer.score)
 
